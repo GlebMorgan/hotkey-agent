68db34a59ed02d1d4e6946f1974ae6887507dfca
Gleb Klukach <gleb.klukach@teltonika.lt>
2026-06-25 00:34:15 +0300

    feat: update list of Gitlab projects

--- agent.py
+++ agent.py
@@ -82,27 +82,69 @@ class Input(ctypes.Structure):
 
 
 PROJECTS = {
+    # T5 Projects
     "fleet/tms/teltonika-tdf": "TDF",
     "fleet/tms/apps/telematics-app": "TAPP",
-    "fleet/tms/apps/daughterboard-application": "DAPP",
-    "fleet/tms/apps/extension-application": "EXTAPP",
-    "fleet/tms/apps/ble-extension-application": "BLEAPP",
     "fleet/tms/apps/can-extension-application": "CANAPP",
+    "fleet/tms/apps/ble-extension-application": "BLEAPP",
+    "fleet/tms/apps/extension-application": "EXTAPP",
+    "fleet/tms/apps/daughterboard-application": "DAPP",
+    "fleet/tms/apps/btsx-application": "BTSX",
     "fleet/tms/apps/teltonika-bootloader": "TBL",
-    "fleet/tms/tools/tms-python-console": "TMS-Console",
     "fleet/tms/libraries/protobuf-communication": "Proto",
     "fleet/tms/libraries/extapp-protobuf-communication": "ExtMCU-Proto",
     "fleet/tms/libraries/extmcu-protobuf-communication": "ExtMCU-Proto",
+    "fleet/tms/platforms/stm32/stm32g0xx_hal_driver": "STM32-HAL",
+
+    # T5 Tools
+    "fleet/tms/tools/ci-cd-cfg": "CI-CD",
+    "fleet/tms/tools/tms-python-console": "TMS-Console",
     "fleet/tms/tools/gitlab-roulette": "GitLab-Roulette",
-    "fleet/fmb/fmb-firmware": "FMB",
-    "fleet/fmb6/fmb6-firmware": "FMB6",
-    "fleet/fmb/tetra-protocol": "TETRA",
+    "jan.smolskij/t5-can-functionality-testing": "CAN-Testing",
+    "fleet/tms/tools/ag3335-airoha-logging-tool-src": "AG-Logger",
+
+    # TCT
     "dotnet/confi/tct": "TCT",
     "fleet/tms/tools/tct-configurations": "TCT-Cfg",
     "fleet/tms/tools/tct-localization": "TCT-Loc",
-    "fleet/fmb/fmb-configurator": "FMB-Configurator",
-    "fleet/fmb/cfg-configurations": "FMB-Cfg",
-    "fleet/fmb/cfg-localization": "FMB-Loc",
+
+    # Factory
+    "fleet/tms/tools/daughterboard-application-testing": "Nightly",
+    "fleet/tms/libraries/factory-testing-configurations": "Factory",
+    "dotnet/factory/tt-evalon/evalon-testing-configurations": "Evalon-Cfg",
+
+    # Protocols
+    "fleet/fmb/tetra-protocol": "TETRA",
+    "dotnet/fota/fota-protocols": "FOTA",
+
+    # FMB6
+    "fleet/fmb6/fmb6-firmware": "FMB6",
+    "fleet/fmb6/fmb6-bootloader": "FMB6-Bootloader",
+    "fleet/fmb6/fmb6-common_ids": "FMB6-IDs",
+    "fleet/fmb6/fmb6-specprojects": "FMB6-Spec",
+    "fleet/fmb6/fmb6-documentation": "FMB6-Docs",
+    "fleet/fmb6/fmb6-tools": "FMB6-Tools",
+    "fleet/fmb6/tiny-usb-lib": "TinyUSB",
+    "fleet/fmb6/fmb6-ble_sdk": "BLE-SDK",
+    "fleet/drivers/FLASH_LIB_FM6": "FLASH-LIB",
+    "fleet/drivers/axl_hal_fm6": "AXL",
+    "fleet/tools/tcp_udp_server": "ServerMain",
+
+    # Configurator
+    "fleet/fmb/fmb-configurator": "CONF",
+    "fleet/fmb/cfg-configurations": "CFG",
+    "fleet/fmb/cfg-localization": "LOC",
+    "fleet/fmb/fmb-configurator-branding": "Branding",
+
+    # FMB
+    "fleet/fmb/fmb-firmware": "FMB",
+    "fleet/fmb/fmb-fmb6-eq-avl-ids": "FMB-AVLIDs",
+    "fleet/fmb/fmb-testtool": "TestTool",
+    "fleet/fmb/fmb-docs-teltonika-lt": "FMB-Docs",
+
+    # Other
+    "gleb.klukach/gk-c-training-workshop-task": "CPP-Workshop",
+    "fleet/tools/rnd_script": "RND-Autofill",
 }
 
 
